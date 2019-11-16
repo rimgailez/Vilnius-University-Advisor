57c8947864f7e612e066045be432eecc79325849
Jokūbas Ruškys <jokubas.ruskys@mif.stud.vu.lt>
2019-11-16 18:51:37 +0300

    Pakeistas StudyProgramme primary key į name

--- VUA_api/Models/StudyProgramme.cs
+++ VUA_api/Models/StudyProgramme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace VUA_api
 {
     public class StudyProgramme : IEquatable<StudyProgramme>, IComparable<StudyProgramme>
     {
-        public int ID { get; set; }
+        [Key]
         public string name { get; set; }
 
         public Faculty faculty { get; set; }
